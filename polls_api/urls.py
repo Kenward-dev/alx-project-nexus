175893e8489e8361f3fc7f40c6bf703854d94b86
Kenward Terhemba <codewithkenward@gmail.com>
2025-08-06 12:11:21 +0100

    modify docs endpoints

--- polls_api/urls.py
+++ polls_api/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.jwt')),
